4ca2936d306bbf9991932b6d64279ea60be44346
AashPointO <aashir24@gmail.com>
2020-12-03 18:49:54 -0800

    Added shell script for cover letter generation

---
+++ generateCoverLetter.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+BUILD_DIR=genCoverLetter_build
+SRC_DIR=genCoverLetter_src
+
+FILE_BASENAME=FarooqiAashirCoverLetter
+
+PDF_COMPILER=lualatex
+
+cd $SRC_DIR # Need to do since cant change lualatex require search path
+clear
+$PDF_COMPILER --output-directory=../$BUILD_DIR ${FILE_BASENAME}.tex
+cd ..
+
+if [ "$1" = "-m" ]
+then
+	echo "Muted Output"
+else
+	clear
+	google-chrome-stable ${BUILD_DIR}/${FILE_BASENAME}.pdf
+fi
